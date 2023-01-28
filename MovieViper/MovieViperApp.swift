dcc459367a7ec40590659809ebc2c1ee53f461e9
dong2810 <dongvip99@gmail.com>
2023-01-28 16:14:09 +0700

    Initial Commit

---
+++ MovieViper/MovieViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieViperApp.swift
+//  MovieViper
+//
+//  Created by đông on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
